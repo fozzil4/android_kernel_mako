f7214c5504484f8a2f94110f25cc0fe044bbbef8
Naeriu <naeriu@localhost.localdomain>
2014-03-28 18:14:24 +0800

    Revert "msm: mipi_lgit: Update lcd_isactive variable at the end of on function"
    This reverts commit 0b47206dc2e028d1f80142059f0d93336226e998.

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -80,6 +80,7 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
+	lcd_isactive = 1;
 	MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 	ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
 			mipi_lgit_pdata->power_on_set_1,
@@ -119,8 +120,6 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 		return ret;
 	}
 
-	lcd_isactive = 1;
-
 	pr_info("%s finished\n", __func__);
 	return 0;
 }
@@ -131,8 +130,6 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 	int ret = 0;
 
 	pr_info("%s started\n", __func__);
-
-	lcd_isactive = 0;
 
 	if (mipi_lgit_pdata->bl_pwm_disable)
 		mipi_lgit_pdata->bl_pwm_disable();
@@ -145,6 +142,7 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
+	lcd_isactive = 0;
 	MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 	ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
 			mipi_lgit_pdata->power_off_set_1,
@@ -246,7 +244,7 @@ static void update_power_data(int index, unsigned int *gamma)
 	 * Only attempt to apply if the LCD is active.
 	 * If it isn't, the device will panic-reboot
 	 */
-	if (lcd_isactive) {
+	if(lcd_isactive) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 		ret = mipi_dsi_cmds_tx(&lgit_tx_buf, pos,
 					mipi_lgit_pdata->power_on_set_size_1);
